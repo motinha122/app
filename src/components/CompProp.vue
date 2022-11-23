a0aabc15197e10a1e493efce830fee01fd5382e3
motinha122 <timota132@gmail.com>
2022-11-23 12:54:31 -0300

    v-for todos

--- src/components/CompProp.vue
+++ src/components/CompProp.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        {{fullname}}
+        {{ fullname }}
+    </div>
+    <div v-for="todo in todos" :key="todo.id">
+        {{todo.title}}
+        <span>{{todo.completed}}</span>
     </div>
 </template>
  
@@ -9,18 +13,53 @@
 export default {
     data() {
         return {
-            user:{
+            user: {
                 firstName: 'Xamp',
                 lastName: 'Son',
-            }
+            },
+            todos: [
+                {
+                    "userId": 1,
+                    "id": 1,
+                    "title": "delectus aut autem",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 2,
+                    "title": "quis ut nam facilis et officia qui",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 3,
+                    "title": "fugiat veniam minus",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 4,
+                    "title": "et porro tempora",
+                    "completed": true
+                },
+                {
+                    "userId": 1,
+                    "id": 5,
+                    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+                    "completed": false
+                }
+            ],
         }
     },
     methods: {
-        
+
     },
     computed: {
-        fullname(){
+        fullname() {
             return `${this.user.firstName} ${this.user.lastName}`
+        },
+        uncompletedTodos(){
+            return this.todos
         }
     }
 }
